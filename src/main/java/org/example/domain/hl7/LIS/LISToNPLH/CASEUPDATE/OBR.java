b02ece1881a7ef12c159c59d77b958dbd20e209b
herradaj <javier.herrada@roche.com>
2025-03-24 16:35:12 +0100

    fix case update

--- src/main/java/org/example/domain/hl7/LIS/LISToNPLH/CASEUPDATE/OBR.java
+++ src/main/java/org/example/domain/hl7/LIS/LISToNPLH/CASEUPDATE/OBR.java
@@ -13,7 +13,11 @@ public class OBR extends org.example.domain.hl7.common.OBR {
     }
 
     public static OBR FromMessage(Order order) {
-        return (OBR) FromMessage(order, new OBR());
+        OBR obr = (OBR) FromMessage(order, new OBR());
+
+        obr.setSampleId(order.getSampleId());
+
+        return obr;
     }
 
     @Override
